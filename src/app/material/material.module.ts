d00319ac5fbd197f98f432b9af69fb074ee70829
desaMts1806 <desaMts1806@MacBook-Pro-de-macpro-5.local>
2018-08-08 12:07:14 -0500

    modificando material demo

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
-import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatExpansionModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule, 
+    MatExpansionModule
   ],
   exports:[
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatExpansionModule
   ],
   declarations: [MaterialDemoComponent]
 })
