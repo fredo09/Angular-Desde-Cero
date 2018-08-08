b00334fdae87545456588ea29fd039a09c48e9f4
desaMts1806 <desaMts1806@MacBook-Pro-de-macpro-5.local>
2018-08-08 10:34:21 -0500

    Trabajando con angular material y tambien con angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialDemoComponent } from './material/material-demo/material-demo.component';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'material-demo', component: MaterialDemoComponent },
+  { path: 'Home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
