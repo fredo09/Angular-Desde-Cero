370e44ff21ea43ac30aa2f1dface1ec1e7d06ea8
desaMts1806 <desaMts1806@MacBook-Pro-de-macpro-5.local>
2018-08-09 11:42:02 -0500

    Agregando listas y input como ejemplos que tiene angula material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
-import { MatToolbarModule, MatButtonModule, MatIconModule, MatExpansionModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatExpansionModule, MatInputModule, MatListModule } from '@angular/material';
 import { MaterialButtonComponent } from './material-button/material-button.component';
+import { MaterialInputComponent } from './material-input/material-input.component';
+import { MaterialListComponent } from './material-list/material-list.component';
 
 @NgModule({
   imports: [
@@ -10,15 +12,19 @@ import { MaterialButtonComponent } from './material-button/material-button.compo
     MatToolbarModule,
     MatButtonModule,
     MatIconModule, 
-    MatExpansionModule
+    MatExpansionModule,
+    MatInputModule,
+    MatListModule
     //MaterialButtonComponent
   ],
   exports:[
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatInputModule,
+    MaterialListComponent
   ],
-  declarations: [MaterialDemoComponent, MaterialButtonComponent]
+  declarations: [MaterialDemoComponent, MaterialButtonComponent, MaterialInputComponent, MaterialListComponent]
 })
 export class MaterialModule { }
