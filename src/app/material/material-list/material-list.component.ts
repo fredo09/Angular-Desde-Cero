517fe1dccb099fc3a0b6238f94b9853a6cf919cb
desaMts1806 <desaMts1806@MacBook-Pro-de-macpro-5.local>
2018-08-10 10:49:24 -0500

    Agregando interpolacion y modelado de objetos con interface y clases

--- src/app/material/material-list/material-list.component.ts
+++ src/app/material/material-list/material-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Estudiante } from '../../shared/model/estudiante';
 
 @Component({
   selector: 'app-material-list',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./material-list.component.css']
 })
 export class MaterialListComponent implements OnInit {
+  
+  estudiante1: Estudiante;
+  estudiante2: Estudiante;
+  estudiante3: Estudiante;
 
   constructor() { }
 
   ngOnInit() {
+    console.log("Inicio el componente");
+    this.estudiante1= {
+      id: 1,
+      nombre: 'Alferdo',
+      ciudad: 'Comitan',
+      fotoUrl: 'https:www.pagina.com/foto.jpg'
+    };
+    this.estudiante2= {
+      id: 2,
+      nombre: 'Pepe',
+      ciudad: 'Comitan'
+    };
+    this.estudiante3= {
+      id: 3,
+      nombre: 'Maria',
+      ciudad: 'Comitan'
+    };
+
+    setTimeout(() => {
+      this.estudiante3 = {
+        id: 3,
+        nombre: 'Maria Guzman',
+        ciudad: 'Trinitaria'
+      }
+    }, 3000)
   }
+
+  
 
 }
